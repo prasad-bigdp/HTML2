6116757112fc8f72d344adf610371b08f4634dbe
prasad-bigdp <bigdp585@gmail.com>
2024-05-06 15:42:48 +0530

    update

---
+++ JS/Code/first/script.js
@@ -0,0 +1,47 @@
+// console.log("hello");
+// var a = 20;
+// a = a + 6;
+// console.log(a)
+// // alert("raj");
+// var b = confirm("Are you sure?");
+// console.log(b)
+// var val1 = prompt("enter the value")
+// var val2 = prompt("enter the value")
+// console.log(val1 + val2);
+
+
+
+//Dynamic data typing
+var a = 25.3256;
+// var b = new Number(25);
+
+console.log(a+1, typeof (a))
+// console.log(b+1, typeof b)
+
+console.log(a.toFixed(3))
+console.log(a.toPrecision(4))
+console.log(a.toString(16))
+console.log(a.valueOf())
+//Math Methodconsole.log()s
+console.log(Math.abs(-10))
+console.log(Math.pow(2,3))
+console.log(Math.sqrt(16))
+console.log(Math.cbrt(27))
+console.log(Math.ceil(3.3))
+console.log(Math.floor(3.8))
+console.log(Math.round(3.5))
+console.log(Math.max(2,3))
+console.log(Math.min(3,2))
+console.log(Math.ceil(Math.random() * 10000))
+/*0.0-1.0*/
+
+
+
+
+
+
+
+
+
+
+/*DATA TYPES*/
