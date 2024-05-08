60b2260dbbadc696dcddee531bbedc927d9e5852
prasad-bigdp <bigdp585@gmail.com>
2024-05-08 15:40:12 +0530

    update

--- JS/Code/first/script.js
+++ JS/Code/first/script.js
@@ -1,39 +1,73 @@
-// console.log("hello");
-// var a = 20;
-// a = a + 6;
-// console.log(a)
-// // alert("raj");
-// var b = confirm("Are you sure?");
-// console.log(b)
-// var val1 = prompt("enter the value")
-// var val2 = prompt("enter the value")
-// console.log(val1 + val2);
+// // console.log("hello");
+// // var a = 20;
+// // a = a + 6;
+// // console.log(a)
+// // // alert("raj");
+// // var b = confirm("Are you sure?");
+// // console.log(b)
+// // var val1 = prompt("enter the value")
+// // var val2 = prompt("enter the value")
+// // console.log(val1 + val2);
 
 
 
-//Dynamic data typing
-var a = 25.3256;
-// var b = new Number(25);
+// //Dynamic data typing
+// var a = 25.3256;
+// // var b = new Number(25);
 
-console.log(a+1, typeof (a))
-// console.log(b+1, typeof b)
+// console.log(a+1, typeof (a))
+// // console.log(b+1, typeof b)
 
-console.log(a.toFixed(3))
-console.log(a.toPrecision(4))
-console.log(a.toString(16))
-console.log(a.valueOf())
-//Math Methodconsole.log()s
-console.log(Math.abs(-10))
-console.log(Math.pow(2,3))
-console.log(Math.sqrt(16))
-console.log(Math.cbrt(27))
-console.log(Math.ceil(3.3))
-console.log(Math.floor(3.8))
-console.log(Math.round(3.5))
-console.log(Math.max(2,3))
-console.log(Math.min(3,2))
-console.log(Math.ceil(Math.random() * 10000))
-/*0.0-1.0*/
+// console.log(a.toFixed(3))
+// console.log(a.toPrecision(4))
+// console.log(a.toString(16))
+// console.log(a.valueOf())
+// //Math Methodconsole.log()s
+// console.log(Math.abs(-10))
+// console.log(Math.pow(2,3))
+// console.log(Math.sqrt(16))
+// console.log(Math.cbrt(27))
+// console.log(Math.ceil(3.3))
+// console.log(Math.floor(3.8))
+// console.log(Math.round(3.5))
+// console.log(Math.max(2,3))
+// console.log(Math.min(3,2))
+// console.log(Math.ceil(Math.random() * 10000))
+// /*0.0-1.0*/
+// var str = "the kalam said \"dream big\""
+// console.log(str, typeof (str))
+// /* template literal */
+// var cost = 200,quantity=5;
+// console.log(` the total cost of items are ${cost * quantity}`)
+// var str="Js is very easye"
+// //String built in Methods
+// console.log(str.length);
+// console.log(str.indexOf('e'))-- first occurence
+// console.log(str.lastIndexOf('e')) -- last occurence
+// console.log(str.charAt(3))
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+// console.log(str.concat(" true"))
+// console.log(str.startsWith("Ja"))-- true/false
+// console.log(str.endsWith("bhatt"))
+// console.log("  x y z  ".trim())
+// console.log("  x y z ".trimStart())
+// console.log(str.split(""))
+// console.log(["js", "is", "good"].join(' '));
+// // prasad ==> dasarp
+// console.log("prasad".split('').reverse().join(''))
+// console.log(str.replaceAll('e', 'a'))
+// console.log(str.match('[A-Z]'))
+// console.log(str.slice(1, 5))
+// /* boolean */
+// var x = new Boolean(true)
+// console.log(x.valueOf(), 5 + +'4')
+
+// var y = null;
+// var z = undefined;
+// console.log(y, z, typeof(y),typeof(z))
+// // null and undefined defines absence of data but undefined also expects future occupation of data
+// console.log( null == undefined)
 
 
 
@@ -44,4 +78,32 @@ console.log(Math.ceil(Math.random() * 10000))
 
 
 
-/*DATA TYPES*/
+
+
+
+
+
+
+// /*DATA TYPES*/
+// Hoisting --> declaration/usage preceeds over initialization 
+debugger;
+console.log(x) //undefined
+ console.log(y) //error not defined
+var x = 20;
+console.log(x);
+var y = 50;
+function display ()
+{
+    var a = 25;
+    console.log(a)
+}
+display()
+
+
+
+
+
+
+
+
+
---
+++ JS/Code/functions/script.js
@@ -0,0 +1,126 @@
+function displayFun ()
+{
+ console.log("helllo world")   
+}
+var displayFun2 = function ()
+{
+    console.log("helllo world")
+}
+// function expression -- function declaring/assigning as a variable
+// a function without a name is anonymous function
+    
+displayFun()
+console.log(displayFun, displayFun2)
+
+    // (function ()
+    // {
+    //     let a = 25;
+    // })(); // IIFE- Immediate Invoke Function Expression -- security
+
+// functions also contains parameters
+function sum (a, b)
+{
+    console.log(a+b)
+}
+sum("raj", "shekhar");
+
+// write a function which takes a string and returns no.of words in it
+//Eg: Js is Very Easy --input
+//output--4
+function noOfWords (str)
+{
+    console.log(str.split(" ").length)
+}
+noOfWords("JS Is Very Easy")
+
+// write a function which returns next values evry time we call it
+function nextValue (num)
+{
+    return ++num;
+}
+var x = nextValue(5);
+console.log(x)
+
+// you can keep one function inside another function
+// nested functions
+function x2 ()
+{
+    var a = 20;
+    function y ()
+    {
+        a++;
+        console.log(a)
+    }
+    return y;
+}
+var result = x2();
+console.log(result)// y whole function
+result()
+result()
+result()
+//closures -- inner functions can remember outer functions values
+//memorization
+
+//callback functions
+
+function mad ()
+{
+    console.log("i'm mad")
+}
+function bad ()
+{
+    console.log("i'm bad")
+    
+}
+function xyz (a,b)
+{
+    a()
+    b()
+}
+xyz(mad, bad)
+// callback hell or pyramid of doom
+// call back function is a function within parameter of another function
+// functions always contain local scope
+
+// operators
+//arithematic -- +,-,*,/,%,**
+//logical  && , ||,!
+//relational >,<,>=,<=,==,===,!=,!==
+//incr,decr ++(pre,post), --
+//assignment += -= *= /= %=, =
+//a+=2===> a=a+2
+//ternary operator==> condition ? st1 : st2 ; -- conditional operator
+//+
+var x=35
+var res = (x > 20) ? 25 : 20;
+console.log(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
